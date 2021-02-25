0fbebafe182774544df73f95228c3319f1125bdc
Akash bisariya <akash.bisariya@nagarro.com>
2021-02-25 09:33:23 +0530

    Initial commit - UserService

---
+++ userservice/src/main/java/model/User.java
@@ -0,0 +1,39 @@
+package model;
+
+public class User {
+	Long userId;
+	String userName;
+	String emailId;
+
+	public User(Long userId, String userName, String emailId) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.emailId = emailId;
+	}
+
+	public String getEmailId() {	
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+}
