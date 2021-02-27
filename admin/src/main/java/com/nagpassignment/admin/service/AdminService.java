2d05f870dc759f9bff262a70cc6508d881b50bd1
Akash bisariya <akash.bisariya@nagarro.com>
2021-02-27 23:43:20 +0530

    Added eureka client for services

---
+++ admin/src/main/java/com/nagpassignment/admin/service/AdminService.java
@@ -0,0 +1,5 @@
+package com.nagpassignment.admin.service;
+
+public class AdminService {
+	
+}
--- provider/src/main/java/com/nagpassignment/provider/service/ServiceProviderServiceImpl.java
+++ provider/src/main/java/com/nagpassignment/provider/service/ServiceProviderServiceImpl.java
@@ -2,8 +2,6 @@ package com.nagpassignment.provider.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.stereotype.Service;
