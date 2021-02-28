b8651f0e8269ab3b26e46edf57db589224a28470
Akash bisariya <akash.bisariya@nagarro.com>
2021-03-01 00:15:45 +0530

    Added APIGateway for services

--- services/src/main/java/com/nagpassignment/services/controller/ServicesController.java
+++ services/src/main/java/com/nagpassignment/services/controller/ServicesController.java
@@ -3,6 +3,7 @@ package com.nagpassignment.services.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.nagpassignment.services.model.ServiceModel;
 import com.nagpassignment.services.service.ServiceInterfaceImpl;
 
 @RestController
+@RequestMapping(value = "/services")
 public class ServicesController {
 
 	@Autowired
